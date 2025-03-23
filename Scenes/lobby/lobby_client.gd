6742a6d2daf4764225f6bcf446a326a18778c623
Prince-in-the-wood <tittaya10407@gmail.com>
2025-03-23 14:01:49 +0700

    refactor: rewrite rpc function to be more readable

--- Scenes/lobby/lobby_client.gd
+++ Scenes/lobby/lobby_client.gd
@@ -5,6 +5,7 @@ const SERVER_PORT = 9000
 const CLIENT_IP = "127.0.0.1"
 const GAME_PORT = 9001
 
+### Create central server connection
 func connect_to_lobby_server(peer: ENetMultiplayerPeer):
 	var err = peer.create_client(SERVER_IP, SERVER_PORT)
 	if err == OK:
@@ -15,43 +16,45 @@ func connect_to_lobby_server(peer: ENetMultiplayerPeer):
 	else:
 		print("Failed to connect to lobby server.")
 
+### RPC - For communicate with central server
+@rpc("any_peer", "call_remote", "reliable")
+func request_to_create_room(ip: String, port: int):
+	pass
+
+@rpc("any_peer", "call_remote", "reliable")
+func request_to_join_room(code: String):
+	pass
+
+@rpc("authority", "call_remote", "reliable")
+func handle_room_created(code: String):
+	print("Room created! Share this code: ", code)
+	_start_game_host_server(GAME_PORT)
+
+@rpc("authority", "call_remote", "reliable")
+func handle_joined_room(is_success: bool, host_ip: String = "", host_port: int = 0):
+	if(is_success):
+		print("Joining game at ", host_ip, ":", host_port)
+		_connect_to_game_host(host_ip, host_port)
+	else:
+		print("Failed to join room. Invalid code or room doesn't exist.")
+
+
+### Function for main scene to call
+func create_room():
+	request_to_create_room.rpc_id(1, CLIENT_IP, GAME_PORT)
+
+func join_room(room_code: String):
+	request_to_join_room.rpc_id(1, room_code)
+
+
+### Private function
 func _on_lobby_connected(peer_id):
 	print("Connected to lobby server")
 
 func _on_lobby_disconnected(peer_id):
 	print("Disconnected from lobby server")
-	
 
-@rpc("any_peer", "call_remote", "reliable")
-func request_create_room(ip: String, port: int):
-	pass
-
-@rpc("any_peer", "call_remote", "reliable")
-func request_join_room(code: String):
-	pass
-
-func create_room():
-	request_create_room.rpc_id(1, CLIENT_IP, GAME_PORT)
-
-func join_room(room_code: String):
-	request_join_room.rpc_id(1, room_code)
-
-@rpc("authority", "call_remote", "reliable")
-func room_created(code: String):
-	print("Room created! Share this code: ", code)
-	start_game_host_server(GAME_PORT)
-
-@rpc("authority", "call_remote", "reliable")
-func join_room_success(host_ip: String, host_port: int):
-	print("Joining game at ", host_ip, ":", host_port)
-	connect_to_game_host(host_ip, host_port)
-
-@rpc("authority", "call_remote", "reliable")
-func join_room_failed():
-	print("Failed to join room. Invalid code or room doesn't exist.")
-
-### If player is host
-func start_game_host_server(port: int):
+func _start_game_host_server(port: int):
 	var peer = ENetMultiplayerPeer.new()
 	var err = peer.create_server(port)
 	if err == OK:
@@ -60,8 +63,7 @@ func start_game_host_server(port: int):
 	else:
 		print("Failed to start game host server.")
 
-### If player is joining
-func connect_to_game_host(ip: String, port: int):
+func _connect_to_game_host(ip: String, port: int):
 	var peer = ENetMultiplayerPeer.new()
 	var err = peer.create_client(ip, port)
 	if err == OK:
--- Scenes/lobby/lobby_server.gd
+++ Scenes/lobby/lobby_server.gd
@@ -12,36 +12,15 @@ var rooms = {}
 
 const SERVER_PORT = 9000
 
+### Create Central Server connection
 func start_lobby_server(peer: ENetMultiplayerPeer):
 	peer.create_server(SERVER_PORT)
 	multiplayer.multiplayer_peer = peer
 	print("Lobby server started on port " + str(SERVER_PORT))
 
-# Generate a random 4-letter room code
-func _generate_room_code() -> String:
-	const CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var code = ""
-	
-	while code == "" or rooms.has(code):
-		for i in 10:
-			code += CHARS[randi() % CHARS.length()]
-		
-	return code
-
-@rpc("authority", "call_remote", "reliable")
-func room_created(code: String):
-	pass
-
-@rpc("authority", "call_remote", "reliable")
-func join_room_success(host_ip: String, host_port: int):
-	pass
-
-@rpc("authority", "call_remote", "reliable")
-func join_room_failed():
-	pass
-
+### RPC - For client - server communication
 @rpc("any_peer", "call_remote", "reliable")
-func request_create_room(ip: String, port: int):
+func request_to_create_room(ip: String, port: int):
 	var peer_id = multiplayer.get_remote_sender_id()
 	
 	var room_code = _generate_room_code()
@@ -54,18 +33,36 @@ func request_create_room(ip: String, port: int):
 
 	print("Room created by peer ", peer_id, "with code: ", room_code)
 	
-	# Send the code back to the host that requested it
-	room_created.rpc_id(peer_id, room_code)
+	handle_room_created.rpc_id(peer_id, room_code)
 
 @rpc("any_peer", "call_remote", "reliable")
-func request_join_room(code: String):
+func request_to_join_room(code: String):
 	var peer_id = multiplayer.get_remote_sender_id()
 	
 	if rooms.has(code) and not rooms[code]["is_other_connected"]:
 		rooms[code]["is_other_connected"] = true
 		var room_info = rooms[code]
 		print("Player ", peer_id, " joining room ", code)
-		join_room_success.rpc_id(peer_id, room_info["host_ip"], room_info["host_port"])
+		handle_joined_room.rpc_id(peer_id, true, room_info["host_ip"], room_info["host_port"])
 	else:
 		print("Player ", peer_id, " failed to join room ", code)
-		join_room_failed.rpc_id(peer_id)
+		handle_joined_room.rpc_id(peer_id,false)
+
+@rpc("authority", "call_remote", "reliable")
+func handle_room_created(code: String):
+	pass
+
+@rpc("authority", "call_remote", "reliable")
+func handle_joined_room(is_success: bool, host_ip: String = "", host_port: int = 0):
+	pass
+	
+### Private function
+func _generate_room_code() -> String:
+	const CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var code = ""
+	
+	while code == "" or rooms.has(code):
+		for i in 10:
+			code += CHARS[randi() % CHARS.length()]
+		
+	return code
