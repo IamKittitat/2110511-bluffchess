061a338e0172c75b606dcf898ed0dfdf8c06c4a7
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2025-03-25 18:58:55 +0700

    refactor: move piece folder inside Asset

--- Scenes/set_up_board.gd
+++ Scenes/set_up_board.gd
@@ -5,24 +5,27 @@ const CELL_WIDTH = 60
 
 const TEXTURE_HOLDER_SETUP = preload("res://Scenes/texture_holder_setup.tscn")
 
-const BLACK_BISHOP = preload("res://Piece/bbishop.png")
-const BLACK_KING = preload("res://Piece/bking.png")
-const BLACK_KNIGHT = preload("res://Piece/bknight.png")
-const BLACK_PAWN = preload("res://Piece/bpawn.png")
-const BLACK_QUEEN = preload("res://Piece/bqueen.png")
-const BLACK_ROOK = preload("res://Piece/brook.png")
-const WHITE_BISHOP = preload("res://Piece/wbishop.png")
-const WHITE_ROOK = preload("res://Piece/wrook.png")
-const WHITE_KING = preload("res://Piece/wking.png")
-const WHITE_KNIGHT = preload("res://Piece/wknight.png")
-const WHITE_PAWN = preload("res://Piece/wpawn.png")
-const WHITE_QUEEN = preload("res://Piece/wqueen.png")
+const BLACK_BISHOP = preload("res://Assets/Piece/black_bishop.png")
+const BLACK_KING = preload("res://Assets/Piece/black_king.png")
+const BLACK_KNIGHT = preload("res://Assets/Piece/black_knight.png")
+const BLACK_PAWN = preload("res://Assets/Piece/black_pawn.png")
+const BLACK_QUEEN = preload("res://Assets/Piece/black_queen.png")
+const BLACK_ROOK = preload("res://Assets/Piece/black_rook.png")
+const BLACK_HIDDEN = preload("res://Assets/Piece/black_hidden.png")
+const WHITE_BISHOP = preload("res://Assets/Piece/white_bishop.png")
+const WHITE_KING = preload("res://Assets/Piece/white_king.png")
+const WHITE_KNIGHT = preload("res://Assets/Piece/white_knight.png")
+const WHITE_PAWN = preload("res://Assets/Piece/white_pawn.png")
+const WHITE_QUEEN = preload("res://Assets/Piece/white_queen.png")
+const WHITE_ROOK = preload("res://Assets/Piece/white_rook.png")
+const WHITE_HIDDEN = preload("res://Assets/Piece/white_hidden.png")
 
 @onready var pieces: Node2D = $Pieces
 @onready var dots: Node2D = $Dots
 
 #Variables
 var board : Array = []
+var hidden_board: Array = []
 var white : bool
 var state : bool
 var moves = []
@@ -37,6 +40,14 @@ func _ready():
 	board.append([0, 0, 0, 0, 0, 0, 0, 0])
 	board.append([-4, -2, -3, -5, -6, -3, -2, -4])
 	board.append([-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1])
+	hidden_board.append([2, 2, 2, 2, 2, 2, 2, 2]) # 0,0 == bottom left
+	hidden_board.append([2, 2, 2, 2, 2, 2, 2, 2])
+	hidden_board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	hidden_board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	hidden_board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	hidden_board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	hidden_board.append([2, 2, 2, 2, 2, 2, 2, 2])
+	hidden_board.append([2, 2, 2, 2, 2, 2, 2, 2])
 	
 	display_board()
 	
@@ -70,4 +81,5 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	GlobalScript.chess_board_data = board
+	GlobalScript.hidden_board_data = hidden_board
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
