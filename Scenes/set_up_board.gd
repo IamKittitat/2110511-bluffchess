9a76a645267b2705b2b5f32b0cba565b83bc6bda
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2025-03-27 20:27:48 +0700

    feat: place piece

--- Scenes/set_up_board.gd
+++ Scenes/set_up_board.gd
@@ -28,6 +28,9 @@ var hidden_board: Array = []
 var is_my_turn: bool
 var play_white : bool
 var state : String = "CHOOSE" # CHOOSE, PLACE 
+var selected_piece: int
+
+var piece_left = [0, 8, 2, 2, 2, 1, 1]
 
 func _ready():
 	play_white = GlobalScript.play_as == "white"
@@ -85,35 +88,66 @@ func display_board():
 					
 			holder.scale = Vector2(0.25, 0.25)
 
+func _input(event):
+	if event is InputEventMouseButton && event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if is_mouse_out(): return
+			var col = snapped(get_global_mouse_position().x - 112, 0) / CELL_WIDTH
+			var row = abs(snapped(get_global_mouse_position().y - 560.6, 0)) / CELL_WIDTH
+			
+			if(row <= 1):
+				if(board[row][col] != 0):
+					piece_left[board[row][col]] += 1
+					
+				board[row][col] = selected_piece
+				piece_left[selected_piece] -= 1
+				selected_piece = 0
+				display_board()
+			
+func is_mouse_out():
+	if get_rect().has_point(to_local(get_global_mouse_position())): return false
+	return true
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
-
 
 func _on_button_2_pressed() -> void:
 	GlobalScript.chess_board_data = board
 	GlobalScript.hidden_board_data = hidden_board
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-
 func _on_king_button_pressed() -> void:
-	pass # Replace with function body.
-
+	if(piece_left[6] == 0): 
+		selected_piece = 0
+	else:
+		selected_piece = 6 if play_white else -6
 
 func _on_queen_button_pressed() -> void:
-	pass # Replace with function body.
-
+	if(piece_left[5] == 0): 
+		selected_piece = 0
+	else:
+		selected_piece = 5 if play_white else -5
 
 func _on_knight_button_pressed() -> void:
-	pass # Replace with function body.
-
+	if(piece_left[2] == 0): 
+		selected_piece = 0
+	else:
+		selected_piece = 2 if play_white else -2
 
 func _on_bishop_button_pressed() -> void:
-	pass # Replace with function body.
-
+	if(piece_left[3] == 0): 
+		selected_piece = 0
+	else:
+		selected_piece = 3 if play_white else -3
 
 func _on_rook_button_pressed() -> void:
-	pass # Replace with function body.
-
+	if(piece_left[4] == 0): 
+		selected_piece = 0
+	else:
+		selected_piece = 4 if play_white else -4
 
 func _on_pawn_button_pressed() -> void:
-	pass # Replace with function body.
+	if(piece_left[1] == 0): 
+		selected_piece = 0
+	else:
+		selected_piece = 1 if play_white else -1
